e1c50dcf2132f84c1a4c355e490ee7bf966a99a3
unknown <ajithponnore@gmail.com>
2023-01-02 17:23:15 +0400

    Movie page components payment modal

--- src/pages/Movie.page.jsx
+++ src/pages/Movie.page.jsx
@@ -1,10 +1,18 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { useParams } from "react-router-dom";
 import MovieLayoutHoc from "../layout/Movie.layout";
+import { MovieContext } from "../context/Movie.context";
+import Slider from "react-slick";
+import { FaCcVisa, FaCcApplePay } from "react-icons/fa";
+import PosterSlider from "../components/PosterSlider/PosterSlider.Component";
+import MovieHero from "../components/MovieHero/MovieHero.component";
+import Cast from "../components/Cast/Cast.Component";
 
 const MoviePage = () => {
   const { id } = useParams();
+
+  const { movie, setMovie } = useContext(MovieContext);
 
   const [cast, setCast] = useState([]);
   const [similarMovies, setSimilarMovies] = useState([]);
@@ -16,7 +24,7 @@ const MoviePage = () => {
       setCast(getCast.data.cast);
     };
     requestCast();
-  }, []);
+  }, [id]);
 
   useEffect(() => {
     const requestSimilarMovies = async () => {
@@ -36,7 +44,176 @@ const MoviePage = () => {
     requestRecommendedMovies();
   }, []);
 
-  return <div>MoviePage</div>;
+  useEffect(() => {
+    const requestMovie = async () => {
+      const getMovieData = await axios.get(`/movie/${id}`);
+      setMovie(getMovieData.data);
+    };
+    requestMovie();
+  }, [id]);
+
+  const settingCast = {
+    infinite: false,
+    speed: 500,
+    slidesToShow: 6,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
+  const settings = {
+    infinite: false,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          initialSlide: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+    ],
+  };
+
+  return (
+    <>
+      <MovieHero />
+      <div className="my-12 container px-4 lg:ml-20 lg:w-2/3">
+        <div className="flex flex-col items-start gap-3">
+          <h1 className="text-gray-800 font-bold text-2xl">About the Movie</h1>
+          <p>{movie.overview}</p>
+        </div>
+
+        <div className="my-8">
+          <hr />
+        </div>
+
+        <div className="my-8">
+          <h2 className="text-gray-800 font-bold text-2xl my-3">
+            Applicable Offers
+          </h2>
+          <div className=" flex flex-col gap-3 lg:flex-row ">
+            <div className="flex items-start gap-2 bg-yellow-100 p-3 border-yellow-400 border-dashed border-2 rounded-md">
+              <div className="w-8 h-8">
+                <FaCcVisa className="w-full h-full" />
+              </div>
+              <div className="flex flex-col items-start">
+                <h3 className="text-gray-700 text-xl font-bold">
+                  Visa Stream Offer
+                </h3>
+                <p className="text-gray-600 ">
+                  Get 50% off upto 150 on all VISA cards on BookMyShow Stream.
+                </p>
+              </div>
+            </div>
+
+            <div className="flex items-start gap-2 bg-yellow-100 p-3 border-yellow-400 border-dashed border-2 rounded-md">
+              <div className="w-8 h-8">
+                <FaCcApplePay className="w-full h-full" />
+              </div>
+              <div className="flex flex-col items-start">
+                <h3 className="text-gray-700 text-xl font-bold">Film Pass</h3>
+                <p className="text-gray-600 ">
+                  Get 30% off upto 150 on all VISA cards on BookMyShow Stream.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* cast slider */}
+        <div className="my-8">
+          <hr />
+        </div>
+
+        {/* Cast Slider */}
+        <div className="my-8">
+          <h2 className="text-gray-800 font-bold text-2xl mb-4">
+            Cast and Crew
+          </h2>
+          <Slider {...settingCast}>
+            {cast.map((castData) => (
+              <Cast
+                image={castData.profile_path}
+                castName={castData.original_name}
+                role={castData.character}
+              />
+            ))}
+          </Slider>
+        </div>
+
+        {/* cast slider */}
+        <div className="my-8">
+          <hr />
+        </div>
+
+        {/* Recomemded Movie slider */}
+        <div className="my-8">
+          <PosterSlider
+            config={settings}
+            title="Recommended Movies"
+            posters={recommendedMovies}
+            isDark={false}
+          />
+        </div>
+
+        {/* Recomemded Movie slider */}
+        <div className="my-8">
+          <PosterSlider
+            config={settings}
+            title="BookMyShow Exclusive"
+            posters={similarMovies}
+            isDark={false}
+          />
+        </div>
+
+        <div className="my-8">
+          <hr />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default MovieLayoutHoc(MoviePage);
